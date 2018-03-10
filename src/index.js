2f39c403578bba11d359d41f76f1e39f5c753c3e
OlyaY <olga.yukhnovets@gmail.com>
2018-03-10 19:48:39 +0300

    smart-calculator: add metods

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.initialValue = initialValue;
+    //this.expression = "";
   }
+
 
   add(number) {
     // your implementation
+    this.initialValue += number;
+    return this;
   }
-  
+
   subtract(number) {
     // your implementation
+    this.initialValue -= number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.initialValue *= number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.initialValue /= number;
+    return this;
   }
-
   pow(number) {
     // your implementation
+    this.initialValue = Math.pow(this.initialValue, number);
+    return this;
   }
+ /* get result() {
+    //return this.initialValue;
+    return initialValue.initialValue;
+  }*/
 }
-
 module.exports = SmartCalculator;
